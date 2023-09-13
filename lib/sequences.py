abd1fe33c5a785cdcec84fdf960b683d4d83f08b
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-09-13 17:31:38 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0 :
+        print ([]) 
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        for n in range():
+            if length == 10:
+                print (n)
